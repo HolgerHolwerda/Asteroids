32c6ff7dfea18b49f4da2c01407c6af811246083
Ammar <Ammar.rfai4@gmail.com>
2021-01-14 14:32:53 +0100

    lavet asteoider færdig

--- Asteroid/asteroids.js
+++ Asteroid/asteroids.js
@@ -1,12 +1,17 @@
 
 function Asteroid(){
     this.pos = createVector(random(width), random(height))
+    this.vel = p5.Vector.random2D();
     this.r = random(5, 20);
     this.total = floor(random(5, 15));
     this.offset = [];
-    for (var i = 0; i < total; i++)
-    this.offset = random (-5, 12)
-
+    for (var i = 0; i < this.total; i++){
+    this.offset[i] = random (-5, 20)
+    }
+    this.update = function() {
+        this.pos.add(this.vel);
+ 
+    }
     this.render = function(){
         push();
         stroke(255);
@@ -18,11 +23,24 @@ function Asteroid(){
             var angle = map(i, 0, this.total, 0, TWO_PI);//TWO_PI = 360 degree
             var r = this.r + this.offset[i]
             var x = r * cos(angle);
-            var y = this.r * sin(angle);
+            var y = r * sin(angle);
             vertex(x, y) 
         }
         endShape(CLOSE);
         pop();
 
+    }
+
+    this.edges = function (){
+     if (this.pos.x > width + this.r)
+       this.pos.x= -this.r;
+    } else if (this.pos.x < -this.r){
+     this.pos.x = width + this.r;
+    } 
+    if (this.pos.y > height + this.r){
+        this.pos.y = -this.r;
+    
+    } else if (this.pos.y < -this.r) {
+        this.pos.y =height + this.r;
     }
 }
