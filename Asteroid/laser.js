74fe489e929fb546fb89ef17ce0c38be8f1b7bce
Holger <holgerholwerda@gmail.com>
2021-01-21 13:59:55 +0100

    laser animation working

--- Asteroid/laser.js
+++ Asteroid/laser.js
@@ -1,6 +1,7 @@
-function Laser(spos){  //spos er skibets position
+function Laser(spos, angle){  //spos er skibets position
     this.pos = createVector(spos.x, spos.y);
-    this.vel = createVector();
+    this.vel = p5.Vector.fromAngle(angle);
+    this.vel.mult(10);
 
     this.update = function() {
         this.pos.add(this.vel);
