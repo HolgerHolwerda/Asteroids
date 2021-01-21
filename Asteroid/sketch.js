48641b6597d3968ca2d9e498b5d5dee95678a61e
Holger <holgerholwerda@gmail.com>
2021-01-21 14:43:58 +0100

    laser working

--- Asteroid/sketch.js
+++ Asteroid/sketch.js
@@ -24,10 +24,11 @@ function draw() {
   for (var i = lasers.length-1; i >= 0; i--) {
     lasers[i].render();
     lasers[i].update();
+   
     for (var j = asteroids.length-1; j >= 0; j--) {
-      if (laser[i].hits(asteroids[j])){     
-        var newAsterods = asteroids[j].breakup();
-        asteroids.push(newAsteroid);
+      if (lasers[i].hits(asteroids[j])){     
+        var newAsteroids = asteroids[j].breakup();
+        //asteroids.push(newAsteroid);
         asteroids.splice(j, 1);
         lasers.splice(i, 1);
         break;
