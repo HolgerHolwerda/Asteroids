3314d1866cd152ecd780caa40ab52f311f61e4f1
Holger <holgerholwerda@gmail.com>
2021-01-21 13:53:20 +0100

    problems with laser solved

--- Asteroid/laser.js
+++ Asteroid/laser.js
@@ -1,16 +1,16 @@
 function Laser(spos){  //spos er skibets position
-    this.pos = createVektor(spos.x, spos.y);
-    this.vel = createVektor();
+    this.pos = createVector(spos.x, spos.y);
+    this.vel = createVector();
 
     this.update = function() {
         this.pos.add(this.vel);
     }
 
     this.render = function() {
-        Push();
+        push();
         stroke(255);
         strokeWeight(4);
-        Point(this.pos.x, this.pos.y);
+        point(this.pos.x, this.pos.y);
         pop();
     }
 }
