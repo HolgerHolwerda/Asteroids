d727967a38ce041e0295ac19b4b87ffcc90518fc
Holger <holgerholwerda@gmail.com>
2021-01-28 13:45:59 +0100

    more stuff added to laser

--- Asteroid/sketch.js
+++ Asteroid/sketch.js
@@ -15,31 +15,32 @@ function setup() {
 function draw() {
   background(20);
   boostbar()
+  
   for (var i = 0; i < asteroids.length; i++) {
     asteroids[i].render();
     asteroids[i].update();
     asteroids[i].edges();
   }
- 
+  
   for (var i = lasers.length-1; i >= 0; i--) {
     lasers[i].render();
     lasers[i].update();
-   
     for (var j = asteroids.length-1; j >= 0; j--) {
       if (lasers[i].hits(asteroids[j])){     
-        var newAsteroids = asteroids[j].breakup();
-        ateroids = asteroids.concat(newAsteroid);
+        if (asteroids[j].r > 10) {
+          var newAsteroids = asteroids[j].breakup();
+          ateroids = asteroids.concat(newAsteroid);
+        } 
         asteroids.splice(j, 1);
         lasers.splice(i, 1);
         break;
       }
     }
   }
-
+  
   ship.render();
   ship.turn();
   ship.update();
   ship.edges();
   ship.movement();
-
 }
